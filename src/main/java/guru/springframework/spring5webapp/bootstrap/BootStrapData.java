9220c3d7be657169f284e8769c76ec72849fae03
Robert J McGinness <robertjmcginness5@gmail.com>
2022-10-04 23:22:48 -0400

    through 17

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,62 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository,
+                         BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author herman = new Author("Herman", "Melville");
+        Book moby = new Book("Moby Dick", "984999098");
+
+        herman.getBooks().add(moby);
+        moby.getAuthors().add(herman);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book j2ee = new Book("J2EE Development without EJB", "978673728");
+
+        rod.getBooks().add(j2ee);
+        j2ee.getAuthors().add(rod);
+
+        Publisher pub = new Publisher("Wiley Interscience",
+                                        "21 Causeway St",
+                                        "Boston",
+                                        "Massachusetts",
+                                        "01225");
+
+        pub.getBooks().add(moby);
+        pub.getBooks().add(j2ee);
+        moby.setPublisher(pub);
+        j2ee.setPublisher(pub);
+        this.publisherRepository.save(pub);
+        this.authorRepository.save(rod);
+        this.authorRepository.save(herman);
+        this.bookRepository.save(j2ee);
+        this.bookRepository.save(moby);
+
+        System.out.println("Started in bootstrap");
+        System.out.println("Number of books: " + this.bookRepository.count());
+        System.out.println("Publishers: " + this.publisherRepository.count());
+    }
+}
---
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -0,0 +1,111 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    private String address1;
+    private String city;
+    private String state;
+    private String zip;
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books = new HashSet<>();
+
+    public Publisher() {
+    }
+
+    public Publisher(String name, String address1, String city, String state, String zip) {
+        this.name = name;
+        this.address1 = address1;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "name='" + name + '\'' +
+                ", address1='" + address1 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
